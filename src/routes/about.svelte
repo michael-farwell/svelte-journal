a9e57ee7fd8d627270a723baa2efd4a69f38a1e9
Michael Farwell <mike@lie-nielsen.com>
2022-03-07 14:55:57 -0500

    initial commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+<!-- about.svelte -->
+<h1>Hi, I'm Mike!</h1>
+
+<p>This is my about page.</p>
---
+++ src/routes/contact.svelte
@@ -0,0 +1,4 @@
+<!-- contact.svelte -->
+<h1>Get in touch</h1>
+
+<p><a href="mailto:me@my.mail">Email me!</a></p>
