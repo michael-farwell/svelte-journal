ef5ebb7fcc945a1ac8c957b4551a57187325a09f
Michael Farwell <mike@lie-nielsen.com>
2022-03-07 15:16:10 -0500

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,20 @@
 import adapter from "@sveltejs/adapter-auto";
+import { mdsvex } from "mdsvex";
 import preprocess from "svelte-preprocess";
 import { sveltePreprocess } from "svelte-preprocess/dist/autoProcess.js";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-  preprocess: preprocess(
-    sveltePreprocess(),
-  ),
-
   kit: {
     adapter: adapter(),
   },
+  extensions: [".svelte", ".md"],
+  preprocess: preprocess(
+    sveltePreprocess(),
+    mdsvex({
+      extensions: [".md"],
+    }),
+  ),
 };
 
 export default config;
