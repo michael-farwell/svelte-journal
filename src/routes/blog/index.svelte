dec6e19f8999182ecbadf6a25b9f0e5c87b1bad4
Michael Farwell <mike@lie-nielsen.com>
2022-03-08 06:44:43 -0500

    finish blog listing

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,4 +1,33 @@
 <!-- blog/index.svelte -->
-<h1>Blog</h1>
+<script lang="ts"
+        context="module">
+  export const load = async ({ fetch }) => {
+    const posts = await fetch("/api/posts.json");
+    const allPosts = await posts.json();
 
-<p>My blog posts will go here eventually</p>
+    return {
+      props: {
+        posts: allPosts,
+      },
+    };
+  };
+</script>
+
+<script lang="ts">
+  export let posts;
+</script>
+
+<h1>Blog</h1>
+<hr>
+<ul>
+  {#each posts as post}
+    <li>
+      <h2>
+        <a href={post.path}>
+          {post.meta.title}
+        </a>
+      </h2>
+      Published: {post.meta.date}
+    </li>
+  {/each}
+</ul>
